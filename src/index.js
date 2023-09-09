57d18551469c3918aa3ec8a84199f0a6e0c09b4f
Saurabh Jaykar <saurabh@kali.kali.org>
2023-09-09 09:48:04 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+ <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+  </>
+);
+
